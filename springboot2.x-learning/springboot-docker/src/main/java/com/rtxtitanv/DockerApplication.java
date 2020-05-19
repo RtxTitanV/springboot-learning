01aa3def39c81c32025ebe1032cdd366510593e0
rtxtitanv <nvidiagtxtitan@outlook.com>
2020-05-19 17:12:03 +0800

    使用Docker部署SpringBoot应用并推送至远程仓库（内网和阿里云）

---
+++ springboot2.x-learning/springboot-docker/src/main/java/com/rtxtitanv/DockerApplication.java
@@ -0,0 +1,27 @@
+package com.rtxtitanv;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * @author rtxtitanv
+ * @version v1.0.0
+ * @name com.rtxtitanv.DockerApplication
+ * @description 主启动类
+ * @date 2020/5/14 14:23
+ */
+@RestController
+@SpringBootApplication
+public class DockerApplication {
+
+    @GetMapping("/")
+    public String home() {
+        return "Hello Docker World";
+    }
+
+    public static void main(String[] args) {
+        SpringApplication.run(DockerApplication.class, args);
+    }
+}
